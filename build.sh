3737ed8ad63c1b7ff3b934c48b502b2618be8bd1
andriyun <andriy.yun@gmail.com>
2016-08-15 15:46:12 +0300

    Add support version of drupal install and workflow

--- build.sh
+++ build.sh
@@ -1,5 +1,12 @@
 #!/bin/sh
 
-cp build/*.make.yml drupal/
-docker-compose exec web drush make profile.make.yml --prepare-install --overwrite -y
-rm drupal/*.make.yml
+# include parse_yaml function
+. scripts/parse_yaml.sh
+# read yaml file
+eval $(parse_yaml docker-compose.yml)
+
+if [ "$services_web_environment_WORKFLOW" = "profile" ]; then
+  cp build/*.make.yml drupal/
+  docker-compose exec web drush make profile.make.yml --prepare-install --overwrite -y
+  rm drupal/*.make.yml
+fi;
--- run.sh
+++ run.sh
@@ -4,8 +4,11 @@ mkdir -p drupal
 
 # remove old containers and start new ones.
 docker-compose stop
-echo "y" |docker-compose rm
+echo "y" | docker-compose rm
 docker-compose up -d
 
 ./build.sh
 ./install.sh
+
+# finish run process with sh session to web container
+docker-compose exec web sh
