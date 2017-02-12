eada42c444bcb304631ed5537afa89caf202edec
Taras Kyryliuk <tkiriluk@gmail.com>
2017-02-12 18:19:16 +0200

    Added optional xdebug to php5.6-fpm container

---
+++ php56-fpm/skilld-php-entrypoint.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+set -e
+
+# Optionally enable Xdebug.
+if [[ $PHP_XDEBUG_ENABLED = 1 ]]; then
+  sed -i 's/^;zend_extension.*/zend_extension = xdebug.so/' /etc/php5/conf.d/xdebug.ini
+fi
+
+exec "$@"
