98bc011ab5b6499d6a968ce411d17d283fd917db
Andy Postnikov <apostnikov@gmail.com>
2017-02-19 00:33:33 +0200

    Remove unrelated files

--- build.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-cp build/*.make.yml drupal/
-docker-compose exec web drush make profile.make.yml --prepare-install --overwrite -y
-rm drupal/*.make.yml
--- clean.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-# remove old containers and start new ones.
-docker-compose stop
-echo "y" | docker-compose rm
