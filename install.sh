a9910c7d216e4c6ae7d60710fabded670028f81f
Andy Postnikov <apostnikov@gmail.com>
2017-02-19 00:33:33 +0200

    Remove unrelated files

--- install.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-#docker-compose exec web composer require commerceguys/addressing commerceguys/intl commerceguys/zone
-
-docker-compose exec web drush si --db-url=mysql://d8:d8@mysql/d8 --account-pass=admin -y
-
-docker-compose exec web drush en default_content -y
